c997ce747ef82942489dec3a074044ed967f37af
prateek <prateek.ashtikar@mastercard.com>
2020-01-08 11:58:13 +0530

    Spring Retry Overview

---
+++ spring-retry-overview/bass-tracker-service/src/main/java/com/example/BassTrackerServiceApplication.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import com.example.client.LakeProfile;
+import com.example.client.LakeProfileClient;
+
+@SpringBootApplication
+public class BassTrackerServiceApplication {
+	private static final Logger log = LoggerFactory.getLogger(BassTrackerServiceApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BassTrackerServiceApplication.class, args);
+	}
+	
+	@Bean
+    public LakeProfileClient lakeProfileClient(RestTemplateBuilder builder,
+                                               @Value("${lakeProfileServiceBaseUrl}") String lakeProfileServiceBaseUrl,
+                                               @Value("${connectTimeout}") int clientConnectTimeout,
+                                               @Value("${readTimeout}") int readTimeout) {
+        return new LakeProfileClient(clientFactory(clientConnectTimeout, readTimeout), lakeProfileServiceBaseUrl);
+    }
+	
+	private HttpComponentsClientHttpRequestFactory clientFactory(int clientConnectTimeout, int readTimeout) {
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+		factory.setReadTimeout(readTimeout);
+		factory.setConnectTimeout(clientConnectTimeout);
+		return factory;
+	}
+
+	@Bean
+    public CommandLineRunner run(LakeProfileClient lakeProfileClient) throws Exception {
+        return args -> {
+            new Thread(() -> {
+                lakeProfileClient.createLakeProfile(
+                        new LakeProfile(1L, "Strawberry Reservoir", "Utah", 40.175397, -111.102157));
+            }).start();
+
+            Thread.sleep(3000);
+
+            LakeProfile strawberryLake = lakeProfileClient.getLakeProfile(1L);
+            log.info(strawberryLake.toString());
+        };
+    }
+}
---
+++ spring-retry-overview/bass-tracker-service/src/main/java/com/example/client/LakeProfileClient.java
@@ -0,0 +1,24 @@
+package com.example.client;
+
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+public class LakeProfileClient {
+    private RestTemplate restTemplate;
+    private String lakeProfileServiceBaseUrl;
+
+    public LakeProfileClient(HttpComponentsClientHttpRequestFactory clientFactory, String lakeProfileServiceBaseUrl) {
+        this.restTemplate = new RestTemplate(clientFactory);
+        this.lakeProfileServiceBaseUrl = lakeProfileServiceBaseUrl;
+    }
+
+    public LakeProfile getLakeProfile(Long id) {
+        String url = lakeProfileServiceBaseUrl + "/lake-profile/" + id;
+        return restTemplate.getForObject(url, LakeProfile.class);
+    }
+
+    public void createLakeProfile(LakeProfile lakeProfile) {
+        String url = lakeProfileServiceBaseUrl + "/lake-profile";
+        restTemplate.postForObject(url, lakeProfile, LakeProfile.class);
+    }
+}
